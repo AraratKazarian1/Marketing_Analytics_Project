fdce5c8a6e1b427868ff15b6e2160392a3260b67
AraratKazarian1 <160218717+AraratKazarian1@users.noreply.github.com>
2024-04-04 09:59:26 +0400

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    author='',
+    description='',
+    name='',
+    version='',
+    packages=find_packages(include=['etl','etl.*']),
+    
+)
